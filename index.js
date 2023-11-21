7851a6b5b6e8a7318b48c4feb0a388813caeca8d
unknown <adetutu.francisb@gmail.com>
2023-11-21 18:04:51 -0500

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+alert('Hello World'); 
+//first comment //
+
+//make the image invisible
+var myVariable = 10
+myVariable=10
+myVar + 5
+myVar - 10
+var myVar = "hello" + ' david'
+document.write('fold')
